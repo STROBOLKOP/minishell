969181ac79f0f5de08bb51fb77ee79271996e281
STROBOLKOP <efret@student.s19.be>
2024-05-11 12:38:36 +0200

    Check_for_builtins changed to use else if tree

--- src/check_for_builtins.c
+++ src/check_for_builtins.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 19:40:59 by pclaus            #+#    #+#             */
-/*   Updated: 2024/05/11 11:43:18 by elias            ###   ########.fr       */
+/*   Updated: 2024/05/11 12:38:05 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ int	check_for_builtins(char *string)
 {
 	if (exact_match(string, "echo"))
 		printf("You have entered the echo builtin\n");
-	if (exact_match(string, "cd"))
+	else if (exact_match(string, "cd"))
 		printf("You have entered the CD builtin\n");
-	if (exact_match(string, "pwd"))
+	else if (exact_match(string, "pwd"))
 		printf("You have entered the PWD builtin\n");
-	if (exact_match(string, "export"))
+	else if (exact_match(string, "export"))
 		printf("You have entered the EXPORT builtin\n");
-	if (exact_match(string, "unset"))
+	else if (exact_match(string, "unset"))
 		printf("You have entered the UNSET builtin\n");
-	if (exact_match(string, "env"))
+	else if (exact_match(string, "env"))
 		printf("You have entered the ENV builtin\n");
-	if (exact_match(string, "exit"))
+	else if (exact_match(string, "exit"))
 		printf("You have entered the EXIT builtin\n");
 	return (0);
 }
