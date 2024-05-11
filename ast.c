c8eeea05134bf9b3b87c733ffbd524df182a41c3
STROBOLKOP <efret@student.s19.be>
2024-05-11 17:59:36 +0200

    AST structure does work, printing and freeing
    This proves that it works and can be written according to norm rules.
    Right now I really dislike how to allocate the full tree as I have to do
    it manually. Carefully making sure all the types are correct and such is
    a pain.
    
    Next up: a testing program to tokenize a string of math instructions.

--- ast.c
+++ ast.c
@@ -6,14 +6,14 @@
 /*   By: elias <efret@student.19.be>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 14:17:10 by elias             #+#    #+#             */
-/*   Updated: 2024/05/11 16:23:35 by elias            ###   ########.fr       */
+/*   Updated: 2024/05/11 17:58:49 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-/* AST according to norm */
-/*
+/* AST according to norm split in all the little parts */
 typedef struct s_ast		t_ast ;
 typedef enum e_ast_tag		t_ast_tag ;
 typedef struct s_ast_nbr	t_ast_nbr ;
@@ -57,7 +57,6 @@ struct s_ast
 	t_ast_tag	tag;
 	t_ast_data	data;
 };
-*/
 
 /* AST Shorter and still fine by the norm */
 /*
@@ -72,7 +71,7 @@ struct s_ast {
 	union {
 		struct s_ast_nbr
 		{
-			int	number;
+			int	nbr;
 		}		s_ast_nbr;
 		struct s_ast_add
 		{
@@ -89,6 +88,7 @@ struct s_ast {
 */
 
 /* AST Best middleground I think */
+/*
 typedef struct s_ast	t_ast;
 
 typedef enum e_ast_tag {
@@ -100,7 +100,7 @@ typedef enum e_ast_tag {
 typedef union u_ast_data {
 	struct s_ast_nbr
 	{
-		int	number;
+		int	nbr;
 	}		s_ast_nbr;
 	struct s_ast_add
 	{
@@ -118,6 +118,7 @@ struct s_ast {
 	t_ast_tag	tag;
 	t_ast_data	data;
 };
+*/
 
 /* AST structure as found online */
 /*
@@ -137,8 +138,93 @@ struct AST {
 };
 */
 
+t_ast	*ast_new_node(t_ast ast)
+{
+	t_ast	*ret;
+
+	ret = malloc(sizeof(t_ast));
+	if (ret)
+		*ret = ast;
+	return (ret);
+}
+
+void	ast_print(t_ast *ptr)
+{
+	if (ptr->tag == AST_NBR)
+	{
+		printf("%d", ptr->data.ast_nbr.nbr);
+		return ;
+	}
+	else if (ptr->tag == AST_ADD)
+	{
+		printf("(");
+		ast_print(ptr->data.ast_add.left);
+		printf(" + ");
+		ast_print(ptr->data.ast_add.right);
+		printf(")");
+		return ;
+	}
+	else if (ptr->tag == AST_MUL)
+	{
+		printf("(");
+		ast_print(ptr->data.ast_mul.left);
+		printf(" * ");
+		ast_print(ptr->data.ast_mul.right);
+		printf(")");
+		return ;
+	}
+	else
+		printf("ERROR\n");
+}
+
+void	ast_free(t_ast *ptr)
+{
+	if (ptr->tag == AST_NBR)
+		;
+	else if (ptr->tag == AST_ADD)
+	{
+		ast_free(ptr->data.ast_add.left);
+		ast_free(ptr->data.ast_add.right);
+	}
+	else if (ptr->tag == AST_MUL)
+	{
+		ast_free(ptr->data.ast_mul.left);
+		ast_free(ptr->data.ast_mul.right);
+	}
+	else
+		printf("ERROR\n");
+	free(ptr);
+}
+
 int	main(void)
 {
-	printf("%d %d %d\n", AST_NBR, AST_ADD, AST_MUL);
+	t_ast	*ast_head;
+
+	ast_head = ast_new_node((t_ast){
+			AST_ADD, {.ast_add = (t_ast_add){
+			ast_new_node((t_ast){
+				AST_NBR, 5}),
+			ast_new_node((t_ast){
+				AST_NBR, 1})}}
+		});
+	ast_print(ast_head);
+	printf("\n");
+	ast_free(ast_head);
+
+	ast_head = ast_new_node((t_ast){AST_MUL,
+		{.ast_mul = (t_ast_mul){
+			ast_new_node((t_ast){AST_ADD,
+			{.ast_add = (t_ast_add){
+				ast_new_node((t_ast){AST_NBR,
+					7}),
+				ast_new_node((t_ast){AST_NBR,
+					5})}}}),
+			ast_new_node((t_ast){
+				AST_NBR,
+				1}
+				)}}});
+	ast_print(ast_head);
+	printf("\n");
+	ast_free(ast_head);
 	return (0);
 }
