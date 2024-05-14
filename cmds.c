cedf259c617a4b289a2d5f23e6a72734d66a77d7
Elias Fret <efret@student.s19.be>
2024-05-14 18:17:32 +0200

    Stuctures to start with AST to test running cmds

--- cmds.c
+++ cmds.c
@@ -4,22 +4,39 @@
 #define PIPE_W 1
 
 #define REDIR_HERE 1 << 0
-#define REDIR_FILE 1 << 1
-#define REDIR_NBR 1 << 2
+
+typedef struct s_ast		t_ast;
+typedef enum e_ast_tag		t_ast_tag;
+typedef struct s_pipe		t_pipe;
+typedef struct s_cmd		t_cmd;
+typedef union u_ast_data	t_ast_data;
+
+enum e_ast_tag
+{
+	AST_PIPE,
+	AST_CMD,
+	AST_END
+};
+
+struct s_pipe
+{
+	t_ast	*left;
+	t_ast	*right;
+};
 
 typedef struct s_redir
 {
-	char	*file;
+	char	*token;
 	int		flags;
 }			t_redir;
 
-typedef struct s_cmd
+struct s_cmd
 {
 	char	*path;
 	char	**cmd_av;
 	t_redir	*redir_in;
 	t_redir	*redir_out;
-}			t_cmd;
+};
 
 int	main(void)
 {
