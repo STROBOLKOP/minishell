6102077bc2dfca7b696278fc832b3e5fb19ca3c8
STROBOLKOP <efret@student.s19.be>
2024-05-31 11:44:37 +0200

    Call lexer and use the resulting token list
    Make the variable t_token *tokens to hold the token list returned by the
    lexer. At the moment this result is just used to be printed, but will be
    passed along to the pipex part of the minishell in future versions.

--- src/interactive.c
+++ src/interactive.c
@@ -6,16 +6,26 @@
 /*   By: efret <efret@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 14:45:03 by efret             #+#    #+#             */
-/*   Updated: 2024/05/24 14:45:18 by efret            ###   ########.fr       */
+/*   Updated: 2024/05/31 11:36:55 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+static void	disp_tokens(t_token *tokens)
+{
+	while (tokens)
+	{
+		printf("Token type %i: %s\n", tokens->tag, tokens->str);
+		tokens = tokens->next;
+	}
+}
+
 void	interactive(void)
 {
 	char	*prompt;
 	char	*line;
+	t_token	*tokens;
 
 	prompt = "minihell> ";
 	while (1)
@@ -27,8 +37,10 @@ void	interactive(void)
 		{
 			add_history(line);
 			printf("You have entered: %s\n", line);
+			tokens = lexer(line);
+			printf("\nIn Interactive\n");
+			disp_tokens(tokens);
 		}
-		check_for_builtins(line);
 		free(line);
 	}
 	exit_handler(0);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 18:32:43 by pclaus            #+#    #+#             */
-/*   Updated: 2024/05/29 13:41:05 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/05/31 11:30:38 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,10 @@
 
 int	main(int ac, char **av, char **envp)
 {
-	char	*prompt;
-	char	*line;
-
 	if (ac > 1)
 		exit_handler(0);
 	(void)av;
 	(void)envp;
-//	interactive();
-	prompt = "minihell> ";
-	while (1)
-	{
-		line = readline(prompt);
-		if (!line)
-			exit_handler(errno);
-		if (line && *line)
-		{
-			printf("%s\n", line);
-			add_history(line);
-			lexer(line);
-		}
-		// check_for_builtins(line);
-	}
+	interactive();
 	return (0);
 }
