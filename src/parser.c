1ad88d768662d3920e2615e3a69adc524dd14e2c
STROBOLKOP <efret@student.s19.be>
2024-05-31 11:41:20 +0200

    Fixed bug that modified the head of token list
    Introduced the iter variable as to not change where the head of the
    token list is pointing to. Previous implementation put the head all the
    way to the last node.

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 19:09:58 by pclaus            #+#    #+#             */
-/*   Updated: 2024/05/29 19:06:01 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/05/31 00:00:20 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,25 @@
 
 void	parser(t_token **token)
 {
-	while ((*token)->str != NULL && token != NULL)
+	t_token	*iter;
+
+	if (!token)
+		return ;
+	iter = *token;
+	while (iter && iter->str != NULL)
 	{
-		if ((ft_strnstr((*token)->str, "|", 1) != NULL))
-			(*token)->tag = PIPE;
-		else if ((ft_strnstr((*token)->str, "<", 1) != NULL)
-				|| (ft_strnstr((*token)->str, ">", 1) != NULL)
-				|| (ft_strnstr((*token)->str, "<<", 2) != NULL)
-				|| (ft_strnstr((*token)->str, ">>", 2) != NULL)
-				|| (ft_strnstr((*token)->str, "END", 3) != NULL))
-			(*token)->tag = REDIRECT;
-		else if ((ft_strnstr((*token)->str, "VAR=", 4)))
-			(*token)->tag = MAKE_VAR;
+		if ((ft_strnstr(iter->str, "|", 1) != NULL))
+			iter->tag = PIPE;
+		else if ((ft_strnstr(iter->str, "<", 1) != NULL)
+				|| (ft_strnstr(iter->str, ">", 1) != NULL)
+				|| (ft_strnstr(iter->str, "<<", 2) != NULL)
+				|| (ft_strnstr(iter->str, ">>", 2) != NULL)
+				|| (ft_strnstr(iter->str, "END", 3) != NULL))
+			iter->tag = REDIRECT;
+		else if ((ft_strnstr(iter->str, "VAR=", 4)))
+			iter->tag = MAKE_VAR;
 		else
-			(*token)->tag = CMD;
-		if ((*token)->next)
-			*token = (*token)->next;
-		else
-			break ;
+			iter->tag = CMD;
+		iter = iter->next;
 	}
 }
