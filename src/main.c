20fc0dee368eac62b7b32bf9badee738818485df
STROBOLKOP <efret@student.s19.be>
2024-06-01 16:37:43 +0200

    Basic example for .minishellrc

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 18:32:43 by pclaus            #+#    #+#             */
-/*   Updated: 2024/06/01 11:42:12 by elias            ###   ########.fr       */
+/*   Updated: 2024/06/01 16:36:51 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,30 @@ void	non_interactive(char *filename, char **envp)
 	exit(0);
 }
 
+void	load_rc(char *filename, char **envp)
+{
+	int		file;
+	char	*line;
+	t_token	*tokens;
+	t_cmd	*cmds;
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return ;
+	line = get_next_line(file);
+	while (line)
+	{
+		cmds = NULL;
+		tokens = lexer(line);
+		make_cmd_list(&cmds, tokens);
+		ft_run_cmds(cmds, envp);
+		free_cmds(&cmds);
+		free_tokens(&tokens);
+		free(line);
+		line = get_next_line(file);
+	}
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	if (ac == 2)
@@ -44,6 +68,7 @@ int	main(int ac, char **av, char **envp)
 	if (ac > 2)
 		exit_handler(0);
 	(void)av;
+	load_rc(".minishellrc", envp);
 	interactive(envp);
 	return (0);
 }
