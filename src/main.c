6869c3b1057929883818635e5a54c9799ee05268
STROBOLKOP <efret@student.s19.be>
2024-06-01 11:33:42 +0200

    Proof of concept to allow a file as argument
    Read the file line by line with get_next_line. Create tokens and
    commands to then execute. Pretty simple to include in the end product I
    think. This could also mean we can allow a simple rc config file for our
    minishell.
    
    Users could set up a .minishellrc to run commands like export and make
    variables and such. Nothing too crazy as we don't need to handle bash
    code like for loops, just allow the things we have to handle from the
    mandatory.

--- src/main.c
+++ src/main.c
@@ -6,15 +6,41 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 18:32:43 by pclaus            #+#    #+#             */
-/*   Updated: 2024/06/01 11:03:30 by elias            ###   ########.fr       */
+/*   Updated: 2024/06/01 11:30:49 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+void	non_interactive(char *filename, char **envp)
+{
+	int		file;
+	char	*line;
+	t_token	*tokens;
+	t_cmd	*cmds;
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return ;
+	line = get_next_line(file);
+	while (line)
+	{
+		cmds = NULL;
+		tokens = lexer(line);
+		make_cmd_list(&cmds, tokens);
+		ft_run_cmds(cmds, envp);
+		free_cmds(&cmds);
+		free_tokens(&tokens);
+		line = get_next_line(file);
+	}
+	exit(0);
+}
+
 int	main(int ac, char **av, char **envp)
 {
-	if (ac > 1)
+	if (ac == 2)
+		non_interactive(av[1], envp);
+	if (ac > 2)
 		exit_handler(0);
 	(void)av;
 	interactive(envp);
