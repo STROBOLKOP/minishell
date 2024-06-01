ac6bc8f492ed22aa243ba8846c16a60265f049f5
STROBOLKOP <efret@student.s19.be>
2024-06-01 11:05:48 +0200

    Forgot to add envp argument in main
    Don't know why it worked when I tested the code. But now it should build
    without compiler errors.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 18:32:43 by pclaus            #+#    #+#             */
-/*   Updated: 2024/05/31 11:30:38 by elias            ###   ########.fr       */
+/*   Updated: 2024/06/01 11:03:30 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	main(int ac, char **av, char **envp)
 	if (ac > 1)
 		exit_handler(0);
 	(void)av;
-	(void)envp;
-	interactive();
+	interactive(envp);
 	return (0);
 }
